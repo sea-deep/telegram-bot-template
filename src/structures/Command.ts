e202a4b5264de7bba08a1a149b9f0f1117df3578
sea-deep <seadeepie@gmail.com>
2026-08-10 21:38:47 +0530

    feat: upgrade command structure with aliases and args

--- src/structures/Command.ts
+++ src/structures/Command.ts
@@ -5,6 +5,12 @@ export interface Command {
   name: string;
   description: string;
   category?: string;
+  aliases?: string[];
+  usage?: string;
+  examples?: string[];
+  args?: boolean;
+  ownerOnly?: boolean;
+  developerOnly?: boolean;
   options?: CommandOptions;
   disabled?: boolean;
   execute: (ctx: Context, bot: Telegraf<Context>, args: string[]) => Promise<void> | void;
