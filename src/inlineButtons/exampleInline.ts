591aea26ee733357bc7caa435807ddc3de09999b
sea-deep <seadeepie@gmail.com>
2026-08-10 21:38:47 +0530

    refactor: migrate example inline button

---
+++ src/inlineButtons/exampleInline.ts
@@ -0,0 +1,14 @@
+import { InlineButton } from "../structures/InlineButton.js";
+import { Logger } from "../helpers/Logger.js";
+
+const exampleInline: InlineButton = {
+  name: /btn_(.+)/,
+  execute: async (ctx) => {
+    const buttonId = ctx.match?.[1] || "unknown";
+    Logger.debug(`User clicked callback button: ${buttonId}`);
+    await ctx.reply(`You clicked button ${buttonId}`);
+    await ctx.answerCbQuery();
+  },
+};
+
+export default exampleInline;
