f419b63c922e2be579357ca791afb8a74e7e752a
sea-deep <seadeepie@gmail.com>
2026-08-10 21:38:47 +0530

    refactor: update sendInline command

---
+++ src/commands/sendInline.ts
@@ -0,0 +1,26 @@
+import { Markup } from "telegraf";
+import { Command } from "../structures/Command.js";
+
+const sendInlineCommand: Command = {
+  name: "sendinline",
+  description: "Inline keyboard buttons example",
+  category: "Interactive",
+  execute: async (ctx, bot, args) => {
+    const firstName = ctx.from?.first_name || "User";
+    await ctx.reply(
+      `Hello ${firstName}!\nThis command demonstrates inline keyboard buttons:`,
+      Markup.inlineKeyboard([
+        [
+          Markup.button.callback("Click Me!", "btn_1"),
+          Markup.button.callback("Don't Click Me!", "btn_2"),
+        ],
+        [
+          Markup.button.callback("Button 3", "btn_3"),
+          Markup.button.callback("Button 4", "btn_4"),
+        ],
+      ])
+    );
+  },
+};
+
+export default sendInlineCommand;
